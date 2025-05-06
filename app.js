8eedaa2a0f148e3084d8a711cb31a50f213469d6
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-06 15:51:07 +0200

    join & split

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const array = [] // literal tablicy
-console.log(array[0]); // undefined
-array[0] = 1
-array[1] = 2
-array[2] = 3
-console.log(array); // [1, 2, 3]
-console.log(array.length) // 3, po array.length = 0 console pokaze aktualna wartosc wiec 0
-array.length = 0 // tak mozna wyczyscic tablice
-console.log(array); // []
+const numbers = [1, 2, 3, 4, 5]
+
+const numbersInString = numbers.join(' ')
+console.log(numbersInString); // 1 2 3 4 5
+
+const numbersFromString = numbersInString.split(' ')
+console.log(numbersFromString); // ['1', '2', '3', '4', '5']
+
