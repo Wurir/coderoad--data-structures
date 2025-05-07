6039b378ced6d0988434d30fe4a96579b5922788
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-07 16:55:50 +0200

    render elements

--- searchApp.js
+++ searchApp.js
@@ -1,4 +1,4 @@
-let names = []
+let names = ['Ala', 'Ela']
 
 const addName = function(newName){
     names = names.concat(newName)
@@ -7,3 +7,77 @@ const addName = function(newName){
 const nameExist = function(name){
     return names.includes(name)
 }
+
+const renderList = function(){
+    const ul = document.createElement('ul')
+
+    for(let i=0; i<names.length; i++){
+
+        const li = document.createElement('li')
+
+        li.innerText = names[i]
+
+        ul.appendChild(li)
+    }
+
+    return ul
+}
+
+const renderNewNameInput = function(){
+    const div = document.createElement('div')
+    const input = document.createElement('input')
+    const button = document.createElement('button')
+
+    input.setAttribute('placeholder', 'Add new name')
+    button.innerText = 'ADD'
+
+    div.appendChild(input)
+    div.appendChild(button)
+
+    return div
+}
+
+const renderSearchInput = function(){
+    const div = document.createElement('div')
+    const input = document.createElement('input')
+
+    input.setAttribute('placeholder', 'Search name')
+
+    div.appendChild(input)
+
+    return div
+}
+
+const renderSearchResult = function(){
+    const p = document.createElement('p')
+    p.innerText = 'result'
+    return p
+}
+
+const render = function(){
+    const div = document.createElement('div')
+
+    const list = renderList ()
+    const newNameInput = renderNewNameInput()
+    const searchInput = renderSearchInput()
+    const searchResult = renderSearchResult()
+
+    div.appendChild(list)
+    div.appendChild(newNameInput)
+    div.appendChild(searchInput)
+    div.appendChild(searchResult)
+
+    return div
+}
+
+const init = function(containerSelector){
+    const container = document.querySelector(containerSelector)
+
+    if(!container) return
+
+    const app = render()
+
+    container.appendChild(app)
+}
+
+init('body')
