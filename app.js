d6ba38806a9fa19d7482f36b8bf772f50184fd40
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-08 16:42:58 +0200

    map()

--- app.js
+++ app.js
@@ -1,51 +1,41 @@
-const names = ['Ala', 'Ola', 'Ela']
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-for (let i = 0; i < names.length; i++) {
-    const name = names[i]
-    console.log(name)
+let numbersMultiplied = []
+
+for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i]
+    const numberMultiplied = number * 2
+
+    numbersMultiplied = numbersMultiplied.concat(numberMultiplied)
 }
 
-for (let i = 0; i < names.length; i++) {
-    const element = names[i]
-    const index = i
-    const array = names
-    console.log(element, index, array)
-}
+console.log(numbersMultiplied)
 
-const result = names.forEach(console.log)
+const numbersMultipliedByMap = numbers.map(function (number) {
+    return number * 2
+})
 
-console.log(result) // it always will be undefined!
+console.log(numbersMultipliedByMap)
 
-const myOwnForEach = function (array, callback) {
+const myOwnMap = function (array, callback) {
+
+    let arrayMapped = []
 
     for (let i = 0; i < array.length; i++) {
         const element = array[i]
         const index = i
-        const array = names
 
-        callback(element, index, array)
+        const result = callback(element, index, array)
+
+        arrayMapped = arrayMapped.concat(result)
     }
 
+    return arrayMapped
+
 }
 
-myOwnForEach(names, console.log)
-
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-let sum = 0
-
-for (let i = 0; i < numbers.length; i++) {
-    const element = numbers[i]
-
-    sum = sum + element
-}
-
-console.log(sum)
-
-let sumForEach = 0
-
-numbers.forEach(function (number, index, array) {
-    sumForEach = sumForEach + number
+const numbersMultipliedByMyOwnMap = myOwnMap(numbers, function (number) {
+    return number * 2
 })
 
-console.log(sumForEach)
+console.log(numbersMultipliedByMyOwnMap)
