6b8288c219a2b499bdff55dd9c2aac40e081a342
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-08 17:42:29 +0200

    filter() & DOM

--- app.js
+++ app.js
@@ -1,21 +1,73 @@
-const names = ['Ala', 'Ola', 'Ela', 'Iza']
+const names = ['Ala', 'Ola', 'Ela', 'Iza', 'Mateusz']
+let searchPhrase = ''
 
-const namesWithoutElement = names.filter(function (element, index, array) {
-    return element !== 'Ela'
-})
-
-console.log(namesWithoutElement)
-
-const removeArrayElement = function (array, indexToRemove) {
-    return array.filter(function (element, index, array) {
-        return index !== indexToRemove
+const appendArray = function (array, container) {
+    array.forEach(function (element) {
+        container.appendChild(element)
     })
 }
 
-const namesWithoutElement2 = removeArrayElement(names, 2)
+const renderListItem = function (name) {
+    const li = document.createElement('li')
 
-console.log(namesWithoutElement2)
+    li.innerText = name
 
-const namesWithoutElement3 = removeArrayElement(names, names.indexOf('Ela'))
+    return li
+}
 
-console.log(namesWithoutElement3)
+const renderList = function (names) {
+
+    const listContainer = document.createElement('ul')
+
+    const listItems = names.map(renderListItem)
+
+    appendArray(listItems, listContainer)
+
+    return listContainer
+
+}
+
+const renderForm = function (searchPhrase, onInput) {
+
+    const form = document.createElement('form')
+    const input = document.createElement('input')
+
+    input.value = searchPhrase
+    
+    setTimeout(
+        function(){ input.focus() },
+        0
+    )
+
+    input.addEventListener(
+        'input',
+        onInput
+    )
+
+    form.appendChild(input)
+
+    return form
+}
+
+const render = function (rootContainer) {
+
+    rootContainer.innerHTML = ''
+
+    const onInput = function (event) {
+        searchPhrase = event.target.value
+        render(rootContainer)
+    }
+
+    const filteredNames = names.filter(function (name) {
+        return name.includes(searchPhrase)
+    })
+
+    const formElement = renderForm(searchPhrase, onInput)
+    const listElement = renderList(filteredNames)
+
+    rootContainer.appendChild(formElement)
+    rootContainer.appendChild(listElement)
+
+}
+
+render(document.body)
